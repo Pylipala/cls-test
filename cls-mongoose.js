fc0146790275dc5ea113c99aa281ab7354bf999b
Lun Liu <lun.liu@oocl.com>
2016-06-21 12:37:58 +0800

    test cls-mongoose with negative result

--- cls-mongoose.js
+++ cls-mongoose.js
@@ -1,25 +1,36 @@
 var mongoose = require('mongoose');
 var cls = require('continuation-local-storage');
+var clsMongoose = require('cls-mongoose');
 
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://cls_test:cls_test@localhost:27017/cls_test');
 
-var Location = mongoose.model('Location', { name: String });
+var db = mongoose.connection;
 
-var xz = new Location({ name: 'Tibetan' });
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('database connected');
+});
+
+var Location = mongoose.model('Location', {name: String});
+
+var xizang = new Location({name: 'Tibetan'});
 
 var ns = cls.createNamespace('session');
+clsMongoose(ns);
 
-ns.run(function(){
-  ns.set('context', 'I am context value, you got it!');
-  xz.save(function (err) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log('saved sucessfully');
-    }
-    var nsGot = cls.getNamespace('session');
-    var vGot = nsGot.get('context');
-    console.log('value got from cls:' + vGot);
-  });
-})
+ns.run(function () {
+    console.log('saving context value');
+    ns.set('context', 'I am context value, you got it!');
+    xizang.save(function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('saved sucessfully');
+        }
+        var nsGot = cls.getNamespace('session');
+        var vGot = nsGot.get('context');
+        console.log('reading context value');
+        console.log('value got from cls:' + vGot);
+    });
+});
 
